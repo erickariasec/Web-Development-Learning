aa4902447d6d56e9601daefad58caf0b87ae6c0e
erickariasec <erickarias1999@hotmail.com>
2022-04-27 12:22:34 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 146 - Data Types in JavaScript, string, numeric, object

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson146_DataTypesInJavaScript/data_types.js
@@ -0,0 +1,17 @@
+/* DATA TYPES IN JAVASCRIPT */
+
+// string data type
+var name = "Erick";
+console.log(name);
+
+// numeric data type
+var number = 1000;
+console.log(number);
+
+// object data type
+var object = {
+    name : "Juan",
+    lastName : "Perez",
+    phoneNumber : "0987654321"
+}
+console.log(object);
