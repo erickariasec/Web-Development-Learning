af3d7be2232f57c9683d77a31ca695d544f6d234
erickariasec <erickarias1999@hotmail.com>
2022-04-27 12:38:50 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 147 - TypeOf in JavaScript, console.log(typeof personName); - it returns the data type

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson147_TypeOfInJavaScript/type_of.js
@@ -0,0 +1,22 @@
+/* DATA TYPES IN JAVASCRIPT */
+
+/* In JavaScript we say that the type of variables are DYNAMIC, since the type of data it stores could be changed at any time. */
+
+// string data type
+var personName = "Erick";
+console.log(typeof personName);
+
+personName = 2000; // Change string to number data type
+console.log(typeof personName);
+
+// numeric data type
+var number = 1000;
+console.log(number);
+
+// object data type
+var object = {
+    name : "Juan",
+    lastName : "Perez",
+    phoneNumber : "0987654321"
+}
+console.log(typeof object);
