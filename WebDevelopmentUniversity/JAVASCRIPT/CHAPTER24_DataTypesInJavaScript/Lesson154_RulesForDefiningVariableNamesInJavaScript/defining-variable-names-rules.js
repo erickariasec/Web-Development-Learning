923f0a570ccfe092d65ff8f4f78b01363ca1652b
erickariasec <erickarias1999@hotmail.com>
2022-04-28 12:30:19 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 154 - Rules for Defining Variable Names in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson154_RulesForDefiningVariableNamesInJavaScript/defining-variable-names-rules.js
@@ -0,0 +1,17 @@
+// ! JavaScript is case-sensitive
+
+let fullName = "Erick Arias";
+let fullname = "Nathaly Villarroel";
+console.log(fullName);
+console.log(fullname);
+
+// ! You can't start variable name with a number
+let 1name;
+
+// * Possible ways to name a variable
+let a1fullName;
+let _fullName;
+let $fullName;
+
+// ! You can't use javascript reserved word names
+let break = 10;
