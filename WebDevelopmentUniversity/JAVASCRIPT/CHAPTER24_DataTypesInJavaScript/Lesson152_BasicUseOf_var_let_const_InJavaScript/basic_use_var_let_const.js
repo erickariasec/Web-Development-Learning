c07539998b0d69f9f895cb6e538e31af210084d9
erickariasec <erickarias1999@hotmail.com>
2022-04-28 12:10:36 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 151 - Basic Use of var, let, const In JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson152_BasicUseOf_var_let_const_InJavaScript/basic_use_var_let_const.js
@@ -0,0 +1,8 @@
+// ! var is not recommendable to use
+
+let firstName;
+firstName = "Carla";
+console.log(firstName);
+
+const lastName = "Jimenez";
+lastName = "Araujo"; // It is not possible because it is constant
