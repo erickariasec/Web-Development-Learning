090386696bf8da3f2b9abd0e8562c8f7d20ee441
erickariasec <erickarias1999@hotmail.com>
2022-04-29 10:24:38 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 156 - Increment and Decrement Operators in JavaScript, ++, --

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson156_IncrementAndDecrementOperatorsInJavaScript/increment-decrement-operators.js
@@ -0,0 +1,25 @@
+let a = 3, b = 2;
+
+// * INCREMENT
+
+// Pre - Increment (++ operator before variable name)
+z = ++a;
+console.log(a);
+console.log(z);
+
+// Post - Increment (++ operator after variable name)
+z = b++;
+console.log(b);
+console.log(z);
+
+// * DECREMENT
+
+// Pre - Decrement (-- operator before variable name)
+z = --a;
+console.log(a);
+console.log(z);
+
+// Post - Decrement (-- operator after variable name)
+z = b--;
+console.log(b);
+console.log(z);
