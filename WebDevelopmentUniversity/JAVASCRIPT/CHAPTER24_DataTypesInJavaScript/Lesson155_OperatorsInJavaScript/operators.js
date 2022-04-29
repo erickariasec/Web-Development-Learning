b2f14b4c2a8e0b18ec3cb44d8bd0f312edc8c694
erickariasec <erickarias1999@hotmail.com>
2022-04-28 23:55:13 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 155 - Operators in JavaScript, (+, -, *, /, %, **)

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson155_OperatorsInJavaScript/operators.js
@@ -0,0 +1,19 @@
+let a = 3, b = 2;
+
+let z = a + b;
+console.log("The result of the sum is: " + z);
+
+z = a - b;
+console.log("The result of the substraction is: " + z);
+
+z = a * b;
+console.log("The result of the multiplication is: " + z);
+
+z = a / b;
+console.log("The result of the division is: " + z);
+
+z = a % b;
+console.log("The result of the modulus is: " + z);
+
+z = a**b;
+console.log("The result of the exponent operator is: " + z);
