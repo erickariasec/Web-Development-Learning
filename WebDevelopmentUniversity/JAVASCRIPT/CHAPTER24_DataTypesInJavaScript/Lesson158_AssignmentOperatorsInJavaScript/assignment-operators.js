2a93496b4417c2083a9e35895ee7453fd5d2e7de
erickariasec <erickarias1999@hotmail.com>
2022-04-29 10:49:50 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 158 - Assignment Operators in JavaScript, +=, -=, *=, /=, %=

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson158_AssignmentOperatorsInJavaScript/assignment-operators.js
@@ -0,0 +1,16 @@
+let a = 1;
+
+a += 3; // a = a + 3
+console.log(a);
+
+a -= 2; // a = a - 2
+console.log(a);
+
+a *= 10; // a = a * 10
+console.log(a);
+
+a /= 5; // a = a / 5
+console.log(a);
+
+a %= 2; // a = a % 2
+console.log(a);
