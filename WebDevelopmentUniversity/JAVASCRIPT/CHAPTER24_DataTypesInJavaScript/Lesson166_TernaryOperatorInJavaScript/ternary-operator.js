6f9d7012dd5b58a26bb09b523644aacdc3facf7b
erickariasec <erickarias1999@hotmail.com>
2022-04-29 11:58:42 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 166 - Ternary Operator in JavaScript, condition ? Expression 1 : Expression 2;

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson166_TernaryOperatorInJavaScript/ternary-operator.js
@@ -0,0 +1,10 @@
+// !    Condition ? Expression 1 : Expression 2;
+// * If Contdition is true, execute Expression 1
+// * If Condition is false, execute Expression 2
+
+let result = (1 > 2) ? "It is TRUE" : "It is FALSE";
+console.log(result);
+
+let number = 9;
+result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+console.log(result);
