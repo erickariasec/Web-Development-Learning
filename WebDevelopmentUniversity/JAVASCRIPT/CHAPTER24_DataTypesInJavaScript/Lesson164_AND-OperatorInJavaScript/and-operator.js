7930b5bb0a89007c8697245104ef481b754b7824
erickariasec <erickarias1999@hotmail.com>
2022-04-29 11:34:42 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 164 - AND Operator in JavaScript, (&&)

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson164_AND-OperatorInJavaScript/and-operator.js
@@ -0,0 +1,8 @@
+let a = 15;
+let minValue = 0, maxValue = 10;
+
+if (a >= minValue && a <= maxValue) {
+    console.log("The number is within the range");
+} else {
+    console.log("The number is out of range")
+}
