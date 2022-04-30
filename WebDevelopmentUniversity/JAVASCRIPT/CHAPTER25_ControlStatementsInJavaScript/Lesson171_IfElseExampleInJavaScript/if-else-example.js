395dd81e4c1ba66537c079f1087bd3cc4cdf429c
erickariasec <erickarias1999@hotmail.com>
2022-04-30 17:44:32 -0500

    Web Development University, JAVASCRIPT, CHAPTER 25 - Control Statements in JavaScript, Lesson 171 - if/else Example in JavaScript, num validation

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER25_ControlStatementsInJavaScript/Lesson171_IfElseExampleInJavaScript/if-else-example.js
@@ -0,0 +1,13 @@
+let num = 3;
+
+if (num == 1) {
+    console.log("Number one");
+} else if (num == 2) {
+    console.log("Number two");
+} else if (num == 3) {
+    console.log("Number three");
+} else if (num == 4) {
+    console.log("Number four");
+} else {
+    console.log("Unknown Number");
+}
