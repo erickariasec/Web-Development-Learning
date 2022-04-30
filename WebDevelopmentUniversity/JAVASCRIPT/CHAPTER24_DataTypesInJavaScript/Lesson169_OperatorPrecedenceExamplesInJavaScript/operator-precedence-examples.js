fe3bf518cbe24ed878b6a61352f03bf326ff8348
erickariasec <erickarias1999@hotmail.com>
2022-04-30 17:29:37 -0500

    Web Development University, JAVASCRIPT, CHAPTER 24 - Data Types in JavaScript, Lesson 169 - Operator Precedence Examples in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER24_DataTypesInJavaScript/Lesson169_OperatorPrecedenceExamplesInJavaScript/operator-precedence-examples.js
@@ -0,0 +1,12 @@
+let x = 5;
+let y = 10;
+let z = ++x + y--;
+console.log(x);
+console.log(y);
+console.log(z);
+
+let result = 4 + 5 * 6 / 3;
+console.log(result);
+
+result = (4+5) * 6 / 3;
+console.log(result);
