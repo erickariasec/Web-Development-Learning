90ee7e4a4cb7ae927559f1b5017334cda922f224
erickariasec <erickarias1999@hotmail.com>
2022-05-02 12:11:12 -0500

    Web Development University, JAVASCRIPT, CHAPTER 27 - Arrays in JavaScript, Lesson 184 - Arrays in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER27_ArraysInJavaScript/Lesson184_ArraysInJavaScript/arrays.js
@@ -0,0 +1,3 @@
+// let cars = new Array('BMW', 'Mercedes Benz', 'Volvo'); //! This is the classic way (not recommendable nowadays)
+const cars = ["BMW", "Mercedes Benz", "Volvo"];
+console.log(cars);
