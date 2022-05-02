df4fcdf60f291c6b5c8f68a53c5ad82ffa38e96a
erickariasec <erickarias1999@hotmail.com>
2022-05-02 11:52:37 -0500

    Web Development University, JAVASCRIPT, CHAPTER 26 - Loops in JavaScript, Lesson 181 - Break Word in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER26_LoopsInJavaScript/Lesson181_BreakWordInJavaScript/break-word.js
@@ -0,0 +1,8 @@
+for (let counter = 0; counter <= 10; counter++) {
+    if (counter % 2 == 0) {
+        console.log(counter);
+        break; // To end a loop
+    } 
+}
+
+console.log("End of For Loop");
