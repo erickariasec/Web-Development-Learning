226bf44fd069f934f17d8880c324da91dbdbf159
erickariasec <erickarias1999@hotmail.com>
2022-05-02 13:10:08 -0500

    Web Development University, JAVASCRIPT, CHAPTER 28 - Functions in JavaScript, Lesson 191 - Expression Type Functions in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER28_FunctionsInJavaScript/Lesson191_ExpressionTypeFunctionsInJavaScript/expression-type-functions.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function myFunction(a, b) {
+  return a + b;
+}
+
+// Call function
+let result = myFunction(2, 3);
+console.log(result);
+
+// Expression Function Declaration
+let sumar = function(a, b) {
+    return a + b;
+}
+
+// Call Expression Function
+result = sumar(1, 2);
+console.log(result);
