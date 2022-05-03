8c555582126317c9f767b224ad40daf70bae77be
erickariasec <erickarias1999@hotmail.com>
2022-05-03 18:20:57 -0500

    Web Development University, JAVASCRIPT, CHAPTER 29 - Objects in JavaScript, Lesson 199 - Objects in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER29_ObjectsInJavaScript/Lesson199_ObjectsInJavaScript/objects.js
@@ -0,0 +1,12 @@
+let person = {
+    firstName: 'Juan',
+    lastName: 'Perez',
+    email: 'jperez@mail.com',
+    age: 28
+}
+
+console.log(person.firstName);
+console.log(person.age);
+console.log(person.email);
+
+console.log(person);
