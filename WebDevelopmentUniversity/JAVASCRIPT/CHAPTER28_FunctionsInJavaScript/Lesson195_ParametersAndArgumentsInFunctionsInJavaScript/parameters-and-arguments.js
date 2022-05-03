d5d302d1869b396b27f349c8f549c12c167e307a
erickariasec <erickarias1999@hotmail.com>
2022-05-03 17:27:26 -0500

    Web Development University, JAVASCRIPT, CHAPTER 28 - Functions in JavaScript, Lesson 195 - Parameters and Arguments in Functions in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER28_FunctionsInJavaScript/Lesson195_ParametersAndArgumentsInFunctionsInJavaScript/parameters-and-arguments.js
@@ -0,0 +1,10 @@
+// Expression Function Declaration
+let sumar = function (a, b) {   // ! Parameters
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    return a + b;
+}
+
+// Call Expression Function
+result = sumar(3, 6); // ! Arguments
+console.log(result);
