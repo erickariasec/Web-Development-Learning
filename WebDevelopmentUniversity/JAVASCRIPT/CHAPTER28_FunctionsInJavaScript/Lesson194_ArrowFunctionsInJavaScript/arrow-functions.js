c6889413d09cfa79222ef3590e6dec4b8f192678
erickariasec <erickarias1999@hotmail.com>
2022-05-03 17:18:00 -0500

    Web Development University, JAVASCRIPT, CHAPTER 28 - Functions in JavaScript, Lesson 194 - Arrow Functions in JavaScript, const sumArrowFunction = (a, b) => a+b;

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER28_FunctionsInJavaScript/Lesson194_ArrowFunctionsInJavaScript/arrow-functions.js
@@ -0,0 +1,13 @@
+// Expression Function Declaration
+let sumar = function (a, b) {
+    return a + b;
+};
+
+// Call Expression Function
+result = sumar(1, 2);
+console.log(result);
+
+// ! Arrow Function
+const sumArrowFunction = (a, b) => a + b;
+result = sumArrowFunction(3, 5);
+console.log(result);
