df9802423c08cc037ce115e7b55a6b4f37583f17
erickariasec <erickarias1999@hotmail.com>
2022-05-03 16:41:02 -0500

    Web Development University, JAVASCRIPT, CHAPTER 28 - Functions in JavaScript, Lesson 192 - Self Invoking Type Functions in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER28_FunctionsInJavaScript/Lesson192_SelfInvokingTypeFunctionsInJavaScript/self-invoking-type-functions.js
@@ -0,0 +1,22 @@
+// Function Declaration
+function myFunction(a, b) {
+  return a + b;
+}
+
+// Call function
+let result = myFunction(2, 3);
+console.log(result);
+
+// Expression Function Declaration
+let sumar = function (a, b) {
+  return a + b;
+};
+
+// Call Expression Function
+result = sumar(1, 2);
+console.log(result);
+
+// ! Self Invoking Function
+(function (a, b) {
+    console.log("Executing the function: " + (a+b));
+})(3, 4);
