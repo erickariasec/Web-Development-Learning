bd3d348beff453ea77d34b378b79f5d016d6d085
erickariasec <erickarias1999@hotmail.com>
2022-05-11 17:53:57 -0500

    Web Development University, JAVASCRIPT, CHAPTER 30 - Classes in JavaScript, Lesson 217 - Inheritance in JavaScript, extends, super

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER30_ClassesInJavaScript/Lesson217_InheritanceInJavaScript/inheritance.js
@@ -0,0 +1,39 @@
+class Person {
+    constructor(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(firstName) {
+        this._firstName = firstName;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(lastName) {
+        this._lastName = lastName;
+    }
+}
+
+class Employee extends Person {
+    constructor(firstName, lastName, department) {
+        super(firstName, lastName); // ! Call Constructor of the Parent Class (Person)
+        this._department = department;
+    }
+    get department() {
+        return this._department;
+    }
+    set department(department) {
+        this._department = department;
+    }
+}
+
+let person1 = new Person("Juan", "Perez");
+console.log(person1);
+
+let employee1 = new Employee("Maria", "Jimenez", "Marketing");
+console.log(employee1);
+console.log(employee1.firstName);
