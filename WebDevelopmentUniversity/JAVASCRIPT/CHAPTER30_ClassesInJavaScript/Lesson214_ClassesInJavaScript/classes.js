585907476ea228e6394e4c8fb321a72d7fab783f
erickariasec <erickarias1999@hotmail.com>
2022-05-11 12:48:07 -0500

    Web Development University, JAVASCRIPT, CHAPTER 30 - Classes in JavaScript, Lesson 214 - Classes in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER30_ClassesInJavaScript/Lesson214_ClassesInJavaScript/classes.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+let person1 = new Person("Juan", "Perez");
+console.log(person1);
+
+let person2 = new Person("Carlos", "Lara");
+console.log(person2);
