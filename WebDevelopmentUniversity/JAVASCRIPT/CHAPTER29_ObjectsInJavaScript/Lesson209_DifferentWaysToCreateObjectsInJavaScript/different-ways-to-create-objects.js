bd9c6e3ac069c60f02c1c19229402168527e9f7e
erickariasec <erickarias1999@hotmail.com>
2022-05-11 11:34:46 -0500

    Web Development University, JAVASCRIPT, CHAPTER 29 - Objects in JavaScript, Lesson 209 - Different Ways to Create Objects in JavaScript

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER29_ObjectsInJavaScript/Lesson209_DifferentWaysToCreateObjectsInJavaScript/different-ways-to-create-objects.js
@@ -0,0 +1,17 @@
+let myObject = new Object();
+let myObject2 = {}; // ! It's the most recommendable to use (simplified syntax)
+
+let myString = new String("Hello");
+let myString2 = "Hola"; // ! It's the most recommendable to use (simplified syntax)
+
+let myNumber = new Number(1);
+let myNumber2 = 1; // ! It's the most recommendable to use (simplified syntax)
+
+let myBoolean = new Boolean(false);
+let myBoolean2 = false; // ! It's the most recommendable to use (simplified syntax)
+
+let myArray = new Array();
+let myArray2 = []; // ! It's the most recommendable to use (simplified syntax)
+
+let myFunction = new Function();
+let myFunction2 = function(){}; // ! It's the most recommendable to use (simplified syntax)
