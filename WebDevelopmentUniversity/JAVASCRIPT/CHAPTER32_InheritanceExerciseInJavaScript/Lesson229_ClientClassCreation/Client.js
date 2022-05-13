89eb2c6d905d14d9d52c2566708581d51776b904
erickariasec <erickarias1999@hotmail.com>
2022-05-13 09:25:31 -0500

    Web Development University, JAVASCRIPT, CHAPTER 32 - Inheritance Exercise in JavaScript, Lesson 229 - Client Class Creation, Person.js Employee.js Client.js

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER32_InheritanceExerciseInJavaScript/Lesson229_ClientClassCreation/Client.js
@@ -0,0 +1,25 @@
+class Client extends Person {
+
+    static counterClients = 0;
+
+    constructor(registrationDate) {
+        this._idClient = ++Client.counterClients;
+        this._registrationDate = registrationDate;
+    }
+
+    get idClient() {
+        return this._registrationDate;
+    }
+
+    get registrationDate() {
+        return this._registrationDate;
+    }
+
+    set registrationDate(registrationDate) {
+        this._registrationDate = registrationDate;
+    }
+
+    toString() {
+        return `${super.toString()} ${this._idClient} ${this._registrationDate}`;
+    }
+}
---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER32_InheritanceExerciseInJavaScript/Lesson229_ClientClassCreation/Employee.js
@@ -0,0 +1,25 @@
+class Employee extends Person {
+    
+    static counterEmployees = 0;
+
+    constructor(salary) {
+        this._idEmployee = ++Employee.counterEmployees;
+        this._salary = salary;
+    }
+
+    get idEmployee() {
+        return this._idEmployee;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    toString() {
+        return `${super.toString()} ${this._idEmployee} ${this._salary}`;
+    }
+}
