5e4076af40eb387c1c87c73a43b785e29c5f32da
erickariasec <erickarias1999@hotmail.com>
2022-05-14 23:25:01 -0500

    Web Development University, JAVASCRIPT, CHAPTER 33 - Sales System with JavaScript, Lesson 233 - Order Class Creation - Part 1, Order.js

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER33_SalesSystemWithJavaScript/Lesson233_OrderClassCreationPart1/Order.js
@@ -0,0 +1,52 @@
+class Product {
+    static counterProducts = 0;
+
+    constructor(productName, price) {
+        this._idProduct = ++Product.counterProducts;
+        this._productName = productName;
+        this._price = price;
+    }
+
+    get idProduct() {
+        return this._idProduct;
+    }
+
+    get productName() {
+        return this._productName;
+    }
+
+    set productName(productName) {
+        this._productName = productName;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(price) {
+        this._price = price;
+    }
+
+    toString() {
+        return `idProduct: ${this._idProduct}, productName: ${this._productName}, price: $${this._price}`;
+    }
+}
+
+class Order {
+    static counterOrders = 0;
+
+    static get MAX_PRODUCTS() {
+        return 5;
+    }
+
+    constructor() {
+        this._idOrder = ++Order.counterOrders;
+        this._products = [];
+        this._counterAddedProducts = 0;
+    }
+}
+
+let product1 = new Product("Pants", 200);
+let product2 = new Product("Shirt", 100);
+console.log(product1.toString());
+console.log(product2.toString());
