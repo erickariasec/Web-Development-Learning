bc3cf3d251a9b948f393708888d6070fb9a8861c
erickariasec <erickarias1999@hotmail.com>
2022-05-16 10:42:12 -0500

    Web Development University, JAVASCRIPT, CHAPTER 34 - PC World Project with JavaScript, Lesson 237 - Mouse Class Creation with JavaScript, Mouse (child class) extends InputDevice

--- WebDevelopmentUniversity/JAVASCRIPT/CHAPTER34_PCWorldProjectWithJavaScript/Lesson237_MouseClassCreationInJavaScript/pc-world.js
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER34_PCWorldProjectWithJavaScript/Lesson237_MouseClassCreationInJavaScript/pc-world.js
@@ -16,3 +16,24 @@ class InputDevice {
         this._brand = brand;
     }
 }
+
+class Mouse extends InputDevice {
+    static counterMouses = 0;
+
+    constructor(inputType, brand) {
+        super(inputType, brand);
+        this._idMouse = ++Mouse.counterMouses;
+    }
+    get idMouse() {
+        return this.idMouse;
+    }
+    toString() {
+        return `Mouse: [idMouse: ${this._idMouse}, inputType: ${this._inputType}, brand: ${this._brand}]`;
+    }
+}
+
+let mouse1 = new Mouse("USB", "Genius");
+console.log(mouse1.toString());
+let mouse2 = new Mouse("Bluetooth", "Dell");
+mouse2.brand = "Genius";
+console.log(mouse2.toString());
