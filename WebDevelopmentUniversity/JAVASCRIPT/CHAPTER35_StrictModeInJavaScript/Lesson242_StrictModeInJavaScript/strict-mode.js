5a5450c3cb43312c6dbb60aaf6e92fb7100b9f50
erickariasec <erickarias1999@hotmail.com>
2022-05-16 12:18:38 -0500

    Web Development University, JAVASCRIPT, CHAPTER 35 - Strict Mode in JavaScript, Lesson 242 - Strict Mode in JavaScript, 'use strict;'

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER35_StrictModeInJavaScript/Lesson242_StrictModeInJavaScript/strict-mode.js
@@ -0,0 +1,10 @@
+"use strict"; // ! Strict mode: All variables must be declared
+
+let x = 10;
+console.log(x);
+
+function myFunction() {
+    "use strict";
+    let y = 15;
+    console.log(y);
+}
