cada351fabe7870317ffb16ed72dd608bceea6d8
erickariasec <erickarias1999@hotmail.com>
2022-05-16 16:34:40 -0500

    Web Development University, JAVASCRIPT, CHAPTER 37 - Error Handling in JavaScript, Lesson 247 - Try Catch Finally Block in JavaScript, try {} catch (error) {} finally {}

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER37_ErrorHandlingInJavaScript/Lesson247_TryCatchFinallyBlockInJavaScript/try-catch-finally-block.js
@@ -0,0 +1,13 @@
+"use strict";
+
+try {
+    let x = 10;
+    // myFunction();
+} catch(error) {
+    console.log(error);
+} finally {
+    console.log("Finish errors review")
+}
+
+
+console.log("continue...");
