430f690ddd378a184472bc5f2ee7184b99360b4b
erickariasec <erickarias1999@hotmail.com>
2022-05-16 17:07:42 -0500

    Web Development University, JAVASCRIPT, CHAPTER 38 - Arrow Functions in JavaScript, Lesson 249 - Arrow Functions in JavaScript, let myArrowFunction = () => {}

---
+++ WebDevelopmentUniversity/JAVASCRIPT/CHAPTER38_ArrowFunctionsInJavaScript/Lesson249_ArrowFunctionsInJavaScript/arrow-functions.js
@@ -0,0 +1,9 @@
+let myFunction = function() {
+    console.log("Greetings from my function");
+}
+
+const myArrowFunction = () => {
+    console.log("Greeting form my arrow function");
+}
+
+myArrowFunction();
