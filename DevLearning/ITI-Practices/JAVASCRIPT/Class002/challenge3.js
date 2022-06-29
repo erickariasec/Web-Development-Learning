279170b43fbcfb5f227218f6204d537718d1ade9
erickariasec <erickarias1999@hotmail.com>
2022-06-29 11:36:49 -0500

    DevLearning, JavaScript, Class002, Challenge 3: Consecutive Sums (description only)

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class002/challenge3.js
@@ -0,0 +1,7 @@
+/*
+Reto #3 - Sumas consecutivas
+Pide al usuario que ingrese dos números y los sume. Después
+pregunta si quiere añadir otro número: si la respuesta es
+afirmativa añádelo al total; en caso contrario finaliza el
+ciclo y muestra el resultado total en pantalla.
+*/
