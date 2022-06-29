ca8c6a86eb7c0581d4aa7ebdb2511276f7971ef8
erickariasec <erickarias1999@hotmail.com>
2022-06-29 11:23:44 -0500

    DevLearning, JavaScript, Class001, booleans (true and false)

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class001/boolean.js
@@ -0,0 +1,17 @@
+// !True and False: Boolean()
+
+// FALSE
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(false));
+
+// TRUE
+console.log(Boolean(" "));
+console.log(Boolean(1));
+console.log(Boolean(5));
+console.log(Boolean(true));
+console.log(Boolean([]));
+console.log(Boolean({}));
