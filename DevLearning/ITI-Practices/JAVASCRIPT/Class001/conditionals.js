a2c610d25d224070434508d437297c799281fa47
erickariasec <erickarias1999@hotmail.com>
2022-06-29 11:25:44 -0500

    DevLearning, JavaScript, Class001, Conditionals: if, else, ternary operator

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class001/conditionals.js
@@ -0,0 +1,13 @@
+// ! Conditionals IF, ELSE, ? true : false
+
+let decision = true;
+
+if (decision) {
+    console.log("You lose");
+} else {
+    console.log("You lose");
+}
+
+// Ternary Operator
+let total = 5 > 7 ? "This is true" : "This is false";
+console.log(total);
