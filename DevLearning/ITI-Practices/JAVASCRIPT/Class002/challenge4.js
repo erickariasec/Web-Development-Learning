01afcd4a3fa0ab0ec5b4ad2c7ef9330934cd645f
erickariasec <erickarias1999@hotmail.com>
2022-06-29 11:37:42 -0500

    DevLearning, JavaScript, Class002, Challenge 4: Guest List (description only)

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class002/challenge4.js
@@ -0,0 +1,9 @@
+/*
+Reto #4 - Lista de invitados
+Estás organizando un banquete al que quieres invitar a tus amigos.
+Crea un programa que pida el nombre de uno de tus amigos, al
+hacerlo aumenta en uno una variable contadora, después pregunta si
+quieres invitar a alguien más: si la respuesta es afirmativa entonces
+pregunta por una persona más;
+en caso contrario cierra el ciclo y muestra en pantalla cuantos invitados son.
+*/
