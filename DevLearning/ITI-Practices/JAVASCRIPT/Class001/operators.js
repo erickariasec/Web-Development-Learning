5a7bdf134133fbc0cd8bc5914685a561513c68ca
erickariasec <erickarias1999@hotmail.com>
2022-06-29 11:24:47 -0500

    DevLearning, JavaScript, Class001, Operators: arithmetic, assignation, comparator, logic, conditional

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class001/operators.js
@@ -0,0 +1,26 @@
+// !Operators: Assignation, Arithmetics, Comparator, Logic
+
+// Arithmetics
+console.log(5 + 2);
+console.log(3 * 2);
+console.log(2 - 1);
+console.log(5 / 2);
+console.log(5 % 3); // Residuo / Modulus
+console.log(5 ** 2); // Potencia
+
+
+// Assignation
+// !false
+var list = [];
+// +=
+// *=
+
+
+// Comparator
+// ==
+// ===
+
+
+// Logic
+// && AND
+// || OR
