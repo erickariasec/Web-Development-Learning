0e9ff705133dfa1f8e46f6a969778a93835ea7c6
erickariasec <erickarias1999@hotmail.com>
2022-06-30 17:11:20 -0500

    DevLearning, JavaScript, Class002, Next Class Research: adding array.map() method concept and examples

--- DevLearning/ITI-Practices/JAVASCRIPT/Class002/next-class-research.js
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class002/next-class-research.js
@@ -1,1 +1,15 @@
 // !Array Manipulation: Map, filter, some, forEach, Find, findIndex
+
+
+// Array.map() method:  creates a new array from the results of calling a function for every element.
+
+const numbers = [4, 9, 16, 25];
+console.log(numbers.map(Math.sqrt));
+
+
+const otherNumbers = [65, 44, 12, 4];
+console.log(otherNumbers.map(multiplyNumberBy10));
+
+function multiplyNumberBy10(num) {
+    return num * 10;
+}
