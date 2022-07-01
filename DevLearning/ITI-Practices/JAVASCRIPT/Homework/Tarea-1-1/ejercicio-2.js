09d8f84b12ce6caab667a000ade322fca4663230
erickariasec <erickarias1999@hotmail.com>
2022-07-01 11:23:06 -0500

    DevLearning, JavaScript, Homework, Tarea-1-1, adding ejercicio-2 JavaScript file

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Homework/Tarea-1-1/ejercicio-2.js
@@ -0,0 +1,10 @@
+// 2. Traduce a código JavaScript las variables del ejemplo anterior y adjunta una captura.
+
+const firstName = "Erick";
+const lastName = "Arias";
+const ITI_UserName = "erick.arias";
+let age = 23;
+const email = "erick.arias@iti.edu.ec";
+const checkAdult = true;
+let moneySaved = 1000;
+let debts = 100;
