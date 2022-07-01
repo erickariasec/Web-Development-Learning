4353e7143dc2c59f39ab6a0026a513993e42bd5a
erickariasec <erickarias1999@hotmail.com>
2022-06-30 23:12:46 -0500

    DevLearning, JavaScript, Class002, Challenge 2: Beyond 42: refactoring solution of the challenge

--- DevLearning/ITI-Practices/JAVASCRIPT/Class002/challenge2.js
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class002/challenge2.js
@@ -4,12 +4,12 @@ hasta que coloque un número mayor a 42. Cuando se cumpla
 esta condición muestra el resultado en pantalla indicando
 esto al usuario y finaliza el ciclo.*/
 
-let number = 0;
-while(number <= 42){
-    const userNumber = Number(prompt("Ingrese un número mayor a 42"));
-    number = userNumber;
-    if(42 < number){
-        console.log(`El número ${number} es mayor que 42`)
-        debugger
+let userNumber = 0;
+let promptAnswer = "";
+
+while(userNumber <= 42) {
+    userNumber = Number(prompt("Enter a number:"));
+    if (userNumber > 42) {
+        promptAnswer = prompt(`The number ${userNumber} is greater than 42! \n\nTHANKS FOR YOUR PARTICIPATION!`);
     }
-};
+}
