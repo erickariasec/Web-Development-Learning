eb352dec2c03e2d724a37f1de3e019fd50b2720f
erickariasec <erickarias1999@hotmail.com>
2022-07-10 15:14:40 -0500

    DevLearning, ITI-Practices, JavaScript, Tests, Test 1, Adding solution to the question 6 about multiplication for loop

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Tests/Test-1/multiplication-loop.js
@@ -0,0 +1,6 @@
+// 6. Realizar con un bucle for la tabla de multiplicar del 4 En la respuesta colocar el link de solución subida a GitHub
+
+for (let i = 1; i <= 10; i++) {
+    let multiplicationResult = 4 * i;
+    console.log(`4 x ${i} = ${multiplicationResult}`);
+}
