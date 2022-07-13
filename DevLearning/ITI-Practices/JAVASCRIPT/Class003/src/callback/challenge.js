32d8e25e0d940d7b8931fce22ea185e025e819ab
erickariasec <erickarias1999@hotmail.com>
2022-07-13 10:41:58 -0500

    DevLearning, ITI-Practices, JavaScript, Class 3, src, callback, challenge.js, adding more comments explanation and fixing fetchData API call and generating new calls

--- DevLearning/ITI-Practices/JAVASCRIPT/Class003/src/callback/challenge.js
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class003/src/callback/challenge.js
@@ -1,14 +1,18 @@
+// ! Traer el paquete de xmlhttprequest
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// ! Generando un String de mi API de Rick
 const API = "https://rickandmortyapi.com/api";
 
+// ! Generar una función para traer los datos
 function fetchData(urlAPI, callback) {
     let xhttp = new XMLHttpRequest();
     // ! Open API connection
     xhttp.open("GET", urlAPI, true);
     // ! Verify connection status
     xhttp.onreadystatechange = function (event) {
+        // ! Validar el estado de la conexión
         if (xhttp.readyState === 4) {
-            // Validate server response
+            // ! Validate server response
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
@@ -17,10 +21,28 @@ function fetchData(urlAPI, callback) {
             }
         }
     }
+    // ! Enviar la solicitud
     xhttp.send();
 }
 
+// Te guardo en memoria heap
+const newArray = [];
+// Llamo a mi function
+// Asincronismo
 fetchData(`${API}/character`, function (error1, data1) {
     if (error1) return console.error(error1);
-    console.log(data1);
+    fetchData(`${API}/character/${data1.results[0].id}`, function(error2, data2){
+        if (error2) return console.error(error2);
+        fetchData(`${API}/location/${data2.id}`, function(error3, data3){
+            if (error3) return console.error(error3);
+            newArray.push(data1.info.count);
+            console.log(newArray);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    })
 })
+
+// 
+console.log(newArray);
