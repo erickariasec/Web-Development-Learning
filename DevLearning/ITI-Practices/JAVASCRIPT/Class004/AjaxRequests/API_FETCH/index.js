27771df944f21e0dae99b52aefd29e0f9df08002
erickariasec <erickarias1999@hotmail.com>
2022-07-15 12:39:03 -0500

    DevLearning ITI-Practices, JavaScript, Class 4, Ajax Requests, API FETCH, index.js, generating API request using fetch()

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class004/AjaxRequests/API_FETCH/index.js
@@ -0,0 +1,10 @@
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
+const HTMLResponse = document.querySelector("#app");
+
+fetch(`${API_URL}/users`)
+    .then((response) => response.json())
+    .then((users) => {
+        const template = users.map(user => `<li>${user.name} 📧 ${user.email}</li>`);
+        HTMLResponse.innerHTML = `<ul>${template}</ul>`;
+    });
