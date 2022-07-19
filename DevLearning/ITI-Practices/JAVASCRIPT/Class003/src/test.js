20331726d6209e0a499f16b6cf8fb6d827e62401
erickariasec <erickarias1999@hotmail.com>
2022-07-18 23:10:10 -0500

    DevLearning, ITI-Practices, JAVASCRIPT, Class 3, src, test.js, practicing some API Calls

--- DevLearning/ITI-Practices/JAVASCRIPT/Class003/src/test.js
+++ DevLearning/ITI-Practices/JAVASCRIPT/Class003/src/test.js
@@ -1,12 +1,32 @@
 import fetch from "node-fetch";
 const API = "https://rickandmortyapi.com/api";
 
+// const fetchData = async (urlAPI) => {
+//     const response = await fetch(urlAPI);
+//     return await response.json();
+// };
+
 const fetchData = (urlAPI) => {
-    return fetch(urlAPI);
-};
+    return new Promise((resolve) => {
+        resolve(fetch(urlAPI).then((res) => res.json()));
+    });
+}
 
-const response = await fetchData(`${API}/character`);
+async function asyncFetchData() {
+    try {
+        const data1 = await fetchData(`${API}/character`);
+        console.log(data1.info.count);
+        //return data1;
 
-const responseJSON = await response.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-console.log(responseJSON);
+asyncFetchData();
+
+// const response = await fetchData(`${API}/character`);
+
+// const responseJSON = await response.json();
+
+// console.log(responseJSON);
