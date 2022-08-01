3871956ff545b2e6b571fe2408e085729a40f8c5
erickariasec <erickarias1999@hotmail.com>
2022-07-31 21:45:01 -0500

    DevLearning, ITI-Practices, JAVASCRIPT, Homework, Tarea 3.2, src, assets, main.js: Generating API call

---
+++ DevLearning/ITI-Practices/JAVASCRIPT/Homework/Tarea-3-2/src/assets/main.js
@@ -0,0 +1,47 @@
+/*
+**
+***
+Generar el consumo de una API (la entregada en clase) y mostrar los videos de Youtube.
+
+*/
+
+// URL API
+const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UCjXAQ-cayM4mIZmUZKMFW_w&part=snippet%2Cid&order=date&maxResults=10";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "30327383ffmshb8a286fb3ebdc83p1e15cfjsn3e35018d52ca",
+        "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+    },
+};
+
+// Funcion de llamado a nuestra API
+const fetchData = async (urlAPI) => {
+    const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    return data;
+}
+
+// Get Videos
+// Auto llamado de una funcion (function())
+(async () => {
+    const videos = await fetchData(API);
+    const content = document.getElementById('content');
+    let view = `
+    ${videos.items.map((vi) => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${vi.snippet.thumbnails.high.url}" alt="" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${vi.snippet.title}
+                </h3>
+            </div>
+        </div>`
+    ).splice(0,8).join('')}`;
+    content.innerHTML = view;
+})();
