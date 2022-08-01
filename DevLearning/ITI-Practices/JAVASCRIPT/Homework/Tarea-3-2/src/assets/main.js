18ac4586ad932bd9cdbc3939f798266f42782e5c
erickariasec <erickarias1999@hotmail.com>
2022-07-31 23:16:20 -0500

    DevLearning, ITI-Practices, JAVASCRIPT, Homework, Tarea 3.2, src, assets, main.js: changing URL API to get top comedy movies

--- DevLearning/ITI-Practices/JAVASCRIPT/Homework/Tarea-3-2/src/assets/main.js
+++ DevLearning/ITI-Practices/JAVASCRIPT/Homework/Tarea-3-2/src/assets/main.js
@@ -6,13 +6,13 @@ Generar el consumo de una API (la entregada en clase) y mostrar los videos de Yo
 */
 
 // URL API
-const API = "https://youtube-v31.p.rapidapi.com/search?channelId=UCjXAQ-cayM4mIZmUZKMFW_w&part=snippet%2Cid&order=date&maxResults=10";
+const API = "https://movies-app1.p.rapidapi.com/api/movies?limit=20&sort=rating&type=movies&year=2021&genres=comedia";
 
 const options = {
     method: "GET",
     headers: {
         "X-RapidAPI-Key": "30327383ffmshb8a286fb3ebdc83p1e15cfjsn3e35018d52ca",
-        "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+        "X-RapidAPI-Host": "movies-app1.p.rapidapi.com",
     },
 };
 
@@ -21,27 +21,31 @@ const fetchData = async (urlAPI) => {
     const response = await fetch(urlAPI, options);
     const data = await response.json();
     return data;
-}
+};
 
 // Get Videos
 // Auto llamado de una funcion (function())
 (async () => {
     const videos = await fetchData(API);
-    const content = document.getElementById('content');
+    const content = document.getElementById("content");
     let view = `
-    ${videos.items.map((vi) => `
+    ${videos.results
+        .map(
+            (vi) => `
         <div class="group relative">
             <div
                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <img src="${vi.snippet.thumbnails.high.url}" alt="" class="w-full">
+                <img src="${vi.image}" alt="" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
                 <h3 class="text-sm text-gray-700">
                     <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${vi.snippet.title}
+                    ${vi.title}
                 </h3>
             </div>
         </div>`
-    ).splice(0,8).join('')}`;
+        )
+        .splice(0, 8)
+        .join("")}`;
     content.innerHTML = view;
 })();
