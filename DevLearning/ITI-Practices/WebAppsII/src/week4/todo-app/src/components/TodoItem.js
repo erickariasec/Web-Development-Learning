96c640436d1884e969bdfba070e7e4784f154c08
erickariasec <erickarias1999@hotmail.com>
2022-11-10 17:44:00 -0500

    DevLearning, ITI-Practices, WebAppsII, src, week 4, TodoItem.js: adding onCompleteBtn & onDeleteBtn functions

--- DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/components/TodoItem.js
+++ DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/components/TodoItem.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./TodoItem.css"
 import { BsFillTrashFill, BsCheckCircle } from "react-icons/bs";
 
+const onCompleteBtn = () => {
+  console.log("Completed")
+}
+
+const onDeleteBtn = () => {
+  console.log("Deleted")
+}
+
 const TodoItem = (props) => {
   return (
     <li className="todo-item">
-      <span className="check"> <BsCheckCircle /> </span>
+      <span className="check" onClick={onCompleteBtn}> <BsCheckCircle /> </span>
       <p className="paragraph">{props.text}</p>
-      <span className="trash"> <BsFillTrashFill /> </span>
+      <span className="trash" onClick={onDeleteBtn}> <BsFillTrashFill /> </span>
     </li>
   );
 }
