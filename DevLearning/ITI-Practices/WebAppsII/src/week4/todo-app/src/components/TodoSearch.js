4ad83956a777bf785ed891bfdbdbc51c41e91175
erickariasec <erickarias1999@hotmail.com>
2022-11-10 17:45:01 -0500

    DevLearning, ITI-Practices, WebAppsII, src, week 4, TodoSearch.js: adding getInputValue function

--- DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/components/TodoSearch.js
+++ DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/components/TodoSearch.js
@@ -1,8 +1,26 @@
-import React from "react";
-import "./TodoSearch.css"
+import "./TodoSearch.css";
 
-const TodoSearch = () => {
-  return <input className="todo-search" type={"text"} placeholder="Search..." />;
-}
+const TodoSearch = ({search, setSearch}) => {
+  
+  // const [value, setValue] = useState("");
+  
+  const getInputValue = (event) => {
+      console.log(event.target.value);
+      let myValue = event.target.value;
+      setSearch(myValue)
+  };
+
+    return (
+        <>
+            <input
+                className="todo-search"
+                type={"text"}
+                placeholder="Search..."
+                onChange={getInputValue}
+            />
+            <p>{search}</p>
+        </>
+    );
+};
 
 export { TodoSearch };
