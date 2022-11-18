7dd95e420f58396d01696e18eecd59c038546810
erickariasec <erickarias1999@hotmail.com>
2022-11-17 23:31:35 -0500

    DevLearning, ITI-Practices, WebAppsII, src, week 4, src, components, Adding Todo Item component

--- DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/components/TodoItem.js
+++ DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/components/TodoItem.js
@@ -2,22 +2,33 @@ import React from "react";
 import "./TodoItem.css"
 import { BsFillTrashFill, BsCheckCircle } from "react-icons/bs";
 
-const onCompleteBtn = () => {
-  console.log("Completed")
-}
-
-const onDeleteBtn = () => {
-  console.log("Deleted")
-}
-
-const TodoItem = (props) => {
-  return (
-    <li className="todo-item">
-      <span className="check" onClick={onCompleteBtn}> <BsCheckCircle /> </span>
-      <p className="paragraph">{props.text}</p>
-      <span className="trash" onClick={onDeleteBtn}> <BsFillTrashFill /> </span>
-    </li>
-  );
+const TodoItem = ({todo, id, onComplete, onDelete }) => {
+    return (
+        <li className="todo-item">
+            <span
+                className={`${
+                    todo.completed === false ? "unchecked" : "checked"
+                }`}
+                onClick={() => onComplete(id)}
+            >
+                {" "}
+                <BsCheckCircle />{" "}
+            </span>
+            <p
+                className="paragraph"
+                style={{
+                    textDecoration: todo.completed ? "line-through 2px" : "",
+                    color: todo.completed ? "#6d5c63" : "",
+                }}
+            >
+                {todo.content}
+            </p>
+            <span className="trash" onClick={() => onDelete(id)}>
+                {" "}
+                <BsFillTrashFill />{" "}
+            </span>
+        </li>
+    );
 }
 
 export {TodoItem}
