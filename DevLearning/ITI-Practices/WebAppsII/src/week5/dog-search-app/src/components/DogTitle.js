2320bee3d78f4d9de41a11484ff9b673f4ef24d1
erickariasec <erickarias1999@hotmail.com>
2022-11-17 23:21:13 -0500

    DevLearning, ITI-Practices, WebAppsII, src, week 5, src, components, Adding Dog Title component

---
+++ DevLearning/ITI-Practices/WebAppsII/src/week5/dog-search-app/src/components/DogTitle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './DogTitle.css'
+
+const DogTitle = () => {
+    return (
+        <>
+            <h2 className="title-app">DOG SEARCH</h2>
+        </>
+    );
+};
+
+export {DogTitle};
