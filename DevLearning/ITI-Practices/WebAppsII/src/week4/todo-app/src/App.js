8a0263ad7a9c2e72a8457b00a47b9c1f00af5c45
erickariasec <erickarias1999@hotmail.com>
2022-11-17 23:32:13 -0500

    DevLearning, ITI-Practices, WebAppsII, src, week 4, src, Adding App.js and App.css

--- DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/App.js
+++ DevLearning/ITI-Practices/WebAppsII/src/week4/todo-app/src/App.js
@@ -7,27 +7,61 @@ import { TodoItem } from "./components/TodoItem";
 import { TodoList } from "./components/TodoList";
 import { TodoSearch } from "./components/TodoSearch";
 
-const todos = [
-    { content: "Learn React Native", completed: true },
-    { content: "Learn Next.js", completed: false },
-    { content: "Learn Angular", completed: false },
-    { content: "Learn Vue.js", completed: false },
-];
 
 function App() {
+    const [todos, setTodos] = useState([
+        { content: "React Native", completed: true },
+        { content: "Next.js", completed: false },
+        { content: "Angular", completed: false },
+        { content: "Vue.js", completed: false },
+    ]);
 
     const [search, setSearch] = useState("");
 
     const totalTODOs = todos.length;
     const completedTODOs = todos.filter((todo) => !!todo.completed).length;
 
+    let searchedTodos = [];
+
+    if (!search.length >= 1) {
+        searchedTodos = todos;
+    } else {
+        searchedTodos = todos.filter(todo => {
+            const todoContent = todo.content.toLowerCase();
+            const searchContent = search.toLowerCase();
+            return todoContent.includes(searchContent);
+        });
+    }
+
+    const completeTODO = (id) => {
+        const newTodos = [...todos];
+        newTodos[id].completed = true;
+        setTodos(newTodos);
+    };
+
+    const deleteTODO = (id) => {
+        const newTodos = [...todos];
+        newTodos.splice(id, 1);
+        setTodos(newTodos);
+    };
+
     return (
         <div className="todo-container">
             <TodoCounter total={totalTODOs} completed={completedTODOs} />
-            <TodoSearch search={search} setSearch={setSearch} totalTODOs={totalTODOs} />
+            <TodoSearch
+                search={search}
+                setSearch={setSearch}
+                totalTODOs={totalTODOs}
+            />
             <TodoList>
-                {todos.map((todo) => (
-                    <TodoItem key={todo.content} text={todo.content} />
+                {searchedTodos.map((todo, id) => (
+                    <TodoItem
+                        key={id}
+                        id={id}
+                        todo={todo}
+                        onComplete={completeTODO}
+                        onDelete={deleteTODO}
+                    />
                 ))}
             </TodoList>
             <TodoButton />
