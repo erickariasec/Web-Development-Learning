69a83d8d114e50cf768fdaef1010c8c757bf772d
erickariasec <erickarias1999@hotmail.com>
2022-11-27 23:15:10 -0500

    Dev Learning, ITI-Practices, express, routes, simplifying code at authors.router.js

--- DevLearning/ITI-Practices/express/routes/authors.router.js
+++ DevLearning/ITI-Practices/express/routes/authors.router.js
@@ -1,35 +1,44 @@
 const express = require("express");
-const { faker } = require("@faker-js/faker");
+const AuthorsService = require("../services/authors.service");
 
 const route = express.Router();
+const service = new AuthorsService();
 
-route.get('/', (req, res) => {
-    const {size} = req.query
-    const limit = size || 10
-    const authors = [];
-    for (let i = 0; i < limit; i++) {
-        authors.push({
-            authorId: faker.datatype.uuid(),
-            name: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-        })
-    }
-    res.json(authors);
-})
+route.get("/", (req, res) => {
+  const authors = service.findAll();
+
+  res.json(authors);
+});
 
 // ! First goes specific routes, then dynamic routes.
 
 route.get("/filter", (req, res) => {
-    res.send("I am a filter route");
+  res.send("I am a filter route");
 });
 
 route.get("/:id", (req, res) => {
-    const { id } = req.params;
-    res.json({
-        authorId: id,
-        name: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-    });
+  const { id } = req.params;
+  const retFind = service.findSingle(id);
+  res.json(retFind);
+});
+
+route.post("/", (req, res) => {
+  const data = req.body;
+  const newAuthor = service.create(data);
+  res.status(201).json(newAuthor);
+});
+
+route.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  const updateAuthor = service.update(id, changes);
+  res.status(201).json(updateAuthor);
+});
+
+route.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deleteAuthor = service.delete(id);
+  res.status(202).json(deleteAuthor);
 });
 
 module.exports = route;
