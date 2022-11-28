14c049bd1d3b5653a11ab50d5e6ca362eba71576
erickariasec <erickarias1999@hotmail.com>
2022-11-27 23:09:32 -0500

    Dev Learning, ITI-Practices, express, services, adding books.service.js

--- DevLearning/ITI-Practices/express/services/books.service.js
+++ DevLearning/ITI-Practices/express/services/books.service.js
@@ -1,15 +1,53 @@
+const { faker } = require("@faker-js/faker");
+
 class BooksService {
-    constructor() {
-        this.generated();
+  constructor() {
+    this.books = [];
+    this.generate();
+  }
+  generate() {
+    for (let i = 0; i < 10; i++) {
+      this.books.push({
+        id: i + 1,
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+      });
     }
-    generated() {
-        // Call database
-    }
-    created() {}
-    find() {}
-    findOne() {}
-    updated() {}
-    deleted() {}
+  }
+
+  findAll() {
+    return this.books;
+  }
+
+  findSingle(id) {
+    return this.books.find((book) => book.id === parseInt(id));
+  }
+
+  create(data) {
+    this.books.push(data);
+    const bookIndex = this.books.length - 1;
+    return {
+      message: "Book Created",
+      data: this.books[bookIndex],
+    };
+  }
+
+  update(id, changes) {
+    const index = this.books.findIndex((book) => book.id === parseInt(id));
+    this.books.fill(changes, index, index + 1);
+    return {
+      message: "Book Updated",
+      data: this.books[index],
+    };
+  }
+
+  delete(id) {
+    const index = this.books.findIndex((book) => book.id === parseInt(id));
+    return {
+      message: "Book Deleted",
+      data: this.books.splice(index, 1),
+    };
+  }
 }
 
 module.exports = BooksService;
