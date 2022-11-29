32fc4d5006bc08a7b9cd032473fcb3c0f39a563d
erickariasec <erickarias1999@hotmail.com>
2022-11-28 23:51:51 -0500

    Dev Learning, ITI-Practices, express, routes, modifying books.router.js

--- DevLearning/ITI-Practices/express/routes/books.router.js
+++ DevLearning/ITI-Practices/express/routes/books.router.js
@@ -4,35 +4,45 @@ const BooksService = require("../services/books.service");
 const route = express.Router();
 const service = new BooksService();
 
+// localhost:3004/api/v1/books
 route.get("/", (req, res) => {
-  // const { size } = req.query;
-  const books = service.findAll();
-
+  const books = service.findAllBooks();
   res.json(books);
 });
 
+//localhost:3004/api/v1/books/:id
 route.get("/:id", (req, res) => {
   const { id } = req.params;
-  const retFind = service.findSingle(id);
-  res.json(retFind);
+  const foundBookById = service.findBookById(id);
+  res.json(foundBookById);
 });
 
+//localhost:3004/api/v1/books/filter/:title
+route.get("/filter/:title", (req, res) => {
+  const { title } = req.params;
+  const foundBookByTitle = service.findBookByTitle(title);
+  res.json(foundBookByTitle);
+});
+
+//localhost:3004/api/v1/books
 route.post("/", (req, res) => {
   const data = req.body;
-  const newBook = service.create(data);
+  const newBook = service.createBook(data);
   res.status(201).json(newBook);
 });
 
+//localhost:3004/api/v1/books/:id
 route.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
-  const updateBook = service.update(id, changes);
+  const updateBook = service.updateBook(id, changes);
   res.status(201).json(updateBook);
 });
 
+//localhost:3004/api/v1/books/:id
 route.delete("/:id", (req, res) => {
   const { id } = req.params;
-  const deleteBook = service.delete(id);
+  const deleteBook = service.deleteBook(id);
   res.status(202).json(deleteBook);
 });
 
