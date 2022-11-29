45b89f6bff91cc23a22da4b3afce0d156fd13227
erickariasec <erickarias1999@hotmail.com>
2022-11-28 23:52:07 -0500

    Dev Learning, ITI-Practices, express, routes, modifying operations.router.js

--- DevLearning/ITI-Practices/express/routes/operations.router.js
+++ DevLearning/ITI-Practices/express/routes/operations.router.js
@@ -5,32 +5,32 @@ const route = express.Router();
 const service = new OperationsService();
 
 route.get("/", (req, res) => {
-  const operations = service.findAll();
+  const operations = service.findAllOperations();
   res.json(operations);
 });
 
 route.get("/:id", (req, res) => {
   const { id } = req.params;
-  const retFind = service.findSingle(id);
-  res.json(retFind);
+  const foundOperationById = service.findOperationById(id);
+  res.json(foundOperationById);
 });
 
 route.post("/", (req, res) => {
   const data = req.body;
-  const newOperation = service.create(data);
+  const newOperation = service.createOperation(data);
   res.status(201).json(newOperation);
 });
 
 route.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
-  const updateOperation = service.update(id, changes);
+  const updateOperation = service.updateOperation(id, changes);
   res.status(201).json(updateOperation);
 });
 
 route.delete("/:id", (req, res) => {
   const { id } = req.params;
-  const deleteOperation = service.delete(id);
+  const deleteOperation = service.deleteOperation(id);
   res.status(202).json(deleteOperation);
 });
 
