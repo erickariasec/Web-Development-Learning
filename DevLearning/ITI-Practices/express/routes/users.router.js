f65d2a012592462c5fa857daf82bbb4ac615dff8
erickariasec <erickarias1999@hotmail.com>
2022-11-28 23:52:22 -0500

    Dev Learning, ITI-Practices, express, routes, modifying users.router.js

--- DevLearning/ITI-Practices/express/routes/users.router.js
+++ DevLearning/ITI-Practices/express/routes/users.router.js
@@ -4,34 +4,46 @@ const UsersService = require("../services/users.service");
 const route = express.Router();
 const service = new UsersService();
 
+// localhost:3004/api/v1/users
 route.get("/", (req, res) => {
-  const users = service.findAll();
+  const users = service.findAllUsers();
 
   res.json(users);
 });
 
+// localhost:3004/api/v1/users/:id
 route.get("/:id", (req, res) => {
   const { id } = req.params;
-  const retFind = service.findSingle(id);
-  res.json(retFind);
+  const foundUserById = service.findUserById(id);
+  res.json(foundUserById);
 });
 
+// localhost:3004/api/v1/users/filter/:genre
+route.get("/filter/:genre", (req, res) => {
+  const { genre } = req.params;
+  const foundUserByGenre = service.findUsersByGenre(genre);
+  res.json(foundUserByGenre);
+});
+
+// localhost:3004/api/v1/users
 route.post("/", (req, res) => {
   const data = req.body;
-  const newUser = service.create(data);
+  const newUser = service.createUser(data);
   res.status(201).json(newUser);
 });
 
+// localhost:3004/api/v1/users/:id
 route.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
-  const updateUser = service.update(id, changes);
+  const updateUser = service.updateUser(id, changes);
   res.status(201).json(updateUser);
 });
 
+// localhost:3004/api/v1/users/:id
 route.delete("/:id", (req, res) => {
   const { id } = req.params;
-  const deleteUser = service.delete(id);
+  const deleteUser = service.deleteUser(id);
   res.status(202).json(deleteUser);
 });
 
