6cb859e3574530b1522bb7e6f574e3404362e476
erickariasec <erickarias1999@hotmail.com>
2022-11-29 01:23:55 -0500

    Dev Learning, ITI-Practices, express, routes, modifying authors.router.js, filtering by last name

--- DevLearning/ITI-Practices/express/routes/authors.router.js
+++ DevLearning/ITI-Practices/express/routes/authors.router.js
@@ -4,30 +4,41 @@ const AuthorsService = require("../services/authors.service");
 const route = express.Router();
 const service = new AuthorsService();
 
+// localhost:3004/api/v1/authors
 route.get("/", (req, res) => {
   const authors = service.findAllAuthors();
-
   res.json(authors);
 });
 
 // ! First goes specific routes, then dynamic routes.
 
+// localhost:3004/api/v1/authors/filter
 route.get("/filter", (req, res) => {
-  res.send("I am a filter route");
+  res.send("I am a filter route for authors");
 });
 
+//localhost:3004/api/v1/authors/filter/:lastName
+route.get("/filter/:lastName", (req, res) => {
+  const { lastName } = req.params;
+  const foundAuthorByLastName = service.findAuthorByLastName(lastName);
+  res.json(foundAuthorByLastName);
+});
+
+// localhost:3004/api/v1/authors/:id
 route.get("/:id", (req, res) => {
   const { id } = req.params;
-  const retFind = service.findSingleAuthor(id);
+  const retFind = service.findAuthorById(id);
   res.json(retFind);
 });
 
+// localhost:3004/api/v1/authors/
 route.post("/", (req, res) => {
   const data = req.body;
   const newAuthor = service.createAuthor(data);
   res.status(201).json(newAuthor);
 });
 
+// localhost:3004/api/v1/authors/:id
 route.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
@@ -35,6 +46,7 @@ route.put("/:id", (req, res) => {
   res.status(201).json(updateAuthor);
 });
 
+// localhost:3004/api/v1/authors/:id
 route.delete("/:id", (req, res) => {
   const { id } = req.params;
   const deleteAuthor = service.deleteAuthor(id);
