c8680b1d21725647e223e2500dba95fa6cbc7d06
erickariasec <erickarias1999@hotmail.com>
2023-05-05 19:44:11 -0500

    Adding formula to calculate triangle rectangle area

--- DevLearning/ITI-Practices/MobileApps/Tarea2/index.js
+++ DevLearning/ITI-Practices/MobileApps/Tarea2/index.js
@@ -3,31 +3,23 @@ function submitEventHandeler(event) {
 
     let inputValueSide1 = inputSide1.value;
     let inputValueSide2 = inputSide1.value;
-    let inputValueSide3 = inputSide1.value;
 
     let areaText = document.querySelector("#display-result");
 
-    let s = (inputValueSide1 + inputValueSide2 + inputValueSide3) / 2;
-    let area = Math.sqrt(
-        s *
-            (s - inputValueSide1) *
-            (s - inputValueSide2) *
-            (s - inputValueSide3)
-    );
+    let area = (inputValueSide1 * inputValueSide2) / 2;
+    console.log(area);
     areaText.textContent = "The Triangle area is : " + area;
-    areaText.style.color = "rgb(27, 118, 172)";
+    areaText.style.color = "#000";
 }
 
 function resetb() {
     event.preventDefault();
     inputSide1.value = "";
     inputSide2.value = "";
-    inputSide3.value = "";
 }
 
 let form = document.querySelector("#todo-input");
 let inputSide1 = document.querySelector("#todo-input #input-side1");
 let inputSide2 = document.querySelector("#todo-input #input-side2");
-let inputSide3 = document.querySelector("#todo-input #input-side3");
 
 form.onsubmit = submitEventHandeler;
